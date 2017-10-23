49cb0bf5e9879b1ef829a1fa2d7024b5ab8a65a8
Jayateerth Kulkarni <dreamjaya88@hotmail.com>
2017-10-23 22:30:24 +1100

    Initial Commit

---
+++ ShoppingCart/ShoppingCart.App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingCart.App.Startup))]
+namespace ShoppingCart.App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
